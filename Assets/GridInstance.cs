c342b8ab7bf313bb87dce3e666a4e6d4dd92185a
Joe Sujin <joesujin@gmail.com>
2020-02-05 11:00:05 +0530

    First push

---
+++ Assets/GridInstance.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridInstance : MonoBehaviour
+{
+    public GameObject Block;
+    public Material Nextstate;
+    public Tile[,] tiles;
+    public int numX = 10;
+    public int numY = 10;
+    public float scl = 1;
+    public bool isChanging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restart();
+        StartCoroutine(ChangeTiles());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //for (int i = 0; i < numX; i++)
+        //{
+        //    for (int j = 0; j < numY; j++)
+        //    {
+        //        if (tiles[i, j].shifted)
+        //        {
+        //            tiles[i, j].currentPosition = tiles[i, j].targetPosition;
+                    
+        //        }
+        //        else
+        //        {
+        //            for (float b = 0; b < 100; b++)
+        //            {
+        //                tiles[i, j].block.GetComponent<Transform>().transform.position = Vector3.Lerp(new Vector3(tiles[i, j].x * scl, -1f * scl, tiles[i, j].y * scl), new Vector3(tiles[i, j].x * scl, 0f, tiles[i, j].y * scl), b * Time.deltaTime);
+        //            }
+        //        }
+
+        //        if (tiles[i, j].currentPosition == tiles[i, j].targetPosition)
+        //        {
+        //            tiles[i, j].shifted = true;
+        //        }
+
+        //    }
+        //}
+    }
+
+    public void restart()
+    {
+        tiles = new Tile[numX, numY];
+        for (int i = 0; i < numX; i++)
+        {
+            for (int j = 0; j < numY; j++)
+            {
+                Tile newTile = new Tile(i, j);
+                tiles[i, j] = newTile;
+            }
+        }
+
+        for (int i = 0; i < numX; i++)
+        {
+            for (int j = 0; j < numY; j++)
+            {
+                int above = j - 1;
+                int below = j + 1;
+                int left = i - 1;
+                int right = i + 1;
+
+                if (above < 0)
+                {
+                    above = numX - 1;
+                }
+                if (below == numY)
+                {
+                    below = 0;
+                }
+                if (left < 0)
+                {
+                    left = numX - 1;
+                }
+                if (right == numX)
+                {
+                    right = 0;
+                }
+
+                tiles[i, j].addNeighbour(tiles[left, above]);
+                tiles[i, j].addNeighbour(tiles[left, j]);
+                tiles[i, j].addNeighbour(tiles[left, below]);
+                tiles[i, j].addNeighbour(tiles[i, below]);
+                tiles[i, j].addNeighbour(tiles[right, below]);
+                tiles[i, j].addNeighbour(tiles[right, j]);
+                tiles[i, j].addNeighbour(tiles[right, above]);
+                tiles[i, j].addNeighbour(tiles[i, above]);
+
+            }
+        }
+
+        for (int i = 0; i < numX; i++)
+        {
+            for (int j = 0; j < numY; j++)
+            {
+                tiles[i, j].block = Instantiate(Block, tiles[i,j].currentPosition*scl, Quaternion.identity);
+            }
+
+        }
+    }
+
+    IEnumerator ChangeTiles()
+    {
+        while(gameObject)
+        {
+            for (int i = 0; i < numX; i++)
+            {
+                for (int j = 0; j < numY; j++)
+                {
+                    tiles[i, j].calcNextState();
+
+
+                }
+            }
+
+
+            for (int i = 0; i < numX; i++)
+            {
+                for (int j = 0; j < numY; j++)
+                {
+                    tiles[i, j].StateShift();
+
+                    if ((tiles[i, j].nextState && tiles[i, j].state) )
+                    {
+                        tiles[i, j].block.GetComponent<MeshRenderer>().material.color = Color.red;
+                    }
+                    else
+                    {
+                        tiles[i, j].block.GetComponent<MeshRenderer>().material.color = Color.white;
+                    }
+
+                    if (tiles[i,j].state)
+                    {
+                        //tiles[i, j].block.GetComponent<Transform>().transform.position = new Vector3(tiles[i, j].x * scl, 0f, tiles[i, j].y * scl);
+
+                        moveBlocks(i, j, true);
+                    }
+                    else
+                    {
+                        //tiles[i, j].block.GetComponent<Transform>().transform.position = new Vector3(tiles[i, j].x * scl, -1f*scl, tiles[i, j].y * scl);
+
+                        moveBlocks(i, j, false);
+                    }
+
+                    
+
+                }
+            }
+            
+            //yield return new WaitUntil(() => isChanging);
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+
+    public void moveBlocks(int i, int j, bool dir)
+    {
+        if (dir)
+        {
+            for (float b = 0; b < 100; b++)
+            {
+                tiles[i, j].block.GetComponent<Transform>().transform.position = Vector3.Lerp(new Vector3(tiles[i, j].x * scl, -1f * scl, tiles[i, j].y * scl), new Vector3(tiles[i, j].x * scl, 0f, tiles[i, j].y * scl), b * Time.deltaTime);
+            }
+        }
+        else
+        {
+            for (float b = 0; b < 100; b++)
+            {
+                tiles[i, j].block.GetComponent<Transform>().transform.position = Vector3.Lerp(new Vector3(tiles[i, j].x * scl, 0f, tiles[i, j].y * scl), new Vector3(tiles[i, j].x * scl, -1f * scl, tiles[i, j].y * scl), b * Time.deltaTime);
+            }
+        }
+    }
+
+
+
+}
+
---
+++ Assets/Tile.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class Tile 
+{
+    public GameObject block;
+    public float x, y;
+    public bool state;
+    public bool nextState;
+    List<Tile> neighbours;
+    public bool shifted;
+    public Vector3 currentPosition;
+    public Vector3 targetPosition;
+
+    public Tile(float ex, float why)
+    {
+        x = ex;
+        y = why;
+        if (Random.Range(0,3) > 1)
+        {
+            nextState = true;
+            targetPosition = new Vector3(x, 0, y);
+        }
+        else
+        {
+            nextState = false;
+            targetPosition = new Vector3(x, -1, y);
+        }
+        state = nextState;
+        currentPosition = targetPosition;
+        neighbours = new List<Tile>();
+    }
+
+    public void addNeighbour(Tile tile)
+    {
+        neighbours.Add(tile);
+    }
+
+    public void calcNextState()
+    {
+        int liveCount = 0;
+        for (int i = 0; i < neighbours.Count; i++)
+        {
+            if(neighbours[i].state == true)
+            {
+                liveCount++;
+            }
+        }
+
+        if (this.state == true)
+        {
+            if(liveCount == 2 || liveCount == 3)
+            {
+                this.nextState = true;
+            }
+            else
+            {
+                this.nextState = false;
+            }
+        }
+        else
+        {
+            if(liveCount == 3)
+            {
+                nextState = true;
+            }
+        }
+    }
+
+    public void StateShift()
+    {
+        this.state = this.nextState;
+        //this.currentPosition = this.targetPosition;
+        this.shifted = false;
+    }
+    
+}
